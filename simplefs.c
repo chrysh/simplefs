eb86f864b149d2284110333ef287936d270beeec
Christina Quast <chrysh.ng+github@gmail.com>
2018-11-03 12:04:16 +0100

    simplefs_mount fails because empty fill_sb
    [  989.894277] Call Trace:
    [  989.894488]  ? deactivate_locked_super+0x2a/0x60
    [  989.894793]  ? mount_bdev+0x193/0x1a0
    [  989.895058]  ? simplefs_mount+0x11/0x25 [simplefs]
    [  989.895363]  ? mount_fs+0x36/0x162
    [  989.895612]  ? vfs_kern_mount.part.40+0x4f/0x100
    [  989.895923]  ? do_mount+0x20a/0xc90
    [  989.897024]  ? _copy_from_user+0x37/0x60
    [  989.897398]  ? memdup_user+0x39/0x60
    [  989.897737]  ? ksys_mount+0xb5/0xd0
    [  989.898065]  ? __x64_sys_mount+0x1c/0x20
    [  989.898418]  ? do_syscall_64+0x43/0xf0
    [  989.898772]  ? entry_SYSCALL_64_after_hwframe+0x44/0xa9

--- simplefs.c
+++ simplefs.c
@@ -2,12 +2,56 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 
-static int __init example_init(void) {
-printk(KERN_INFO "Hello, World!\n");
-return 0;
+#define DEBUG 1
+#include <linux/dcache.h>
+#include <linux/fs.h>
+
+static struct dentry *simplefs_mount(struct file_system_type *fs_type,
+                     int flags, const char *dev_name, void *data);
+
+static struct file_system_type simplefs_type = {
+    .owner = THIS_MODULE,
+	.name = "simplefs",
+	.mount = simplefs_mount,
+//	.kill_sb = kill_litter_super,
+};
+
+int simplefs_fill_super(struct super_block *sb, void *data, int silent)
+{
+    printk(KERN_INFO "Fill in superblock for simplefs\n");
+    return 0;
 }
+
+static struct dentry *simplefs_mount(struct file_system_type *fs_type,
+                     int flags, const char *dev_name,
+                     void *data)
+{
+    struct dentry *ret;
+
+    ret = mount_bdev(fs_type, flags, dev_name, data, simplefs_fill_super);
+
+    if (unlikely(IS_ERR(ret)))
+        printk(KERN_ERR "Error mounting simplefs");
+    else
+        printk(KERN_INFO "simplefs is succesfully mounted on [%s]\n",
+               dev_name);
+
+    return ret;
+}
+
+static int __init example_init(void) {
+    int err = 0;
+    printk(KERN_INFO "Hello, World!\n");
+    err = register_filesystem(&simplefs_type);
+    if (err) {
+        printk(KERN_ERR "simplefs: unable to register filesystem (err %i)\n", err);
+    }
+    return 0;
+}
+
 static void __exit example_exit(void) {
-printk(KERN_INFO "Goodbye, World!\n");
+    printk(KERN_INFO "Goodbye, World!\n");
+    unregister_filesystem(&simplefs_type);
 }
 
 module_init(example_init);
